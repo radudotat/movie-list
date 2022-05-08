8a61e376f3d032321e2ef22de45a4be9c21fcfa0
Admin <admin.push@hackerrank.workspace>
2022-05-08 14:06:24 +0000

    Auto save changes before Switch to Local IDE
    Commit by HackerRank

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
-import React  from 'react';
+import React from 'react';
 import './App.css';
 import 'h8k-components';
+import { useEffect, useCallback, useState } from 'react';
+
 
 const title = "Movie List";
+const apiEndpoint = 'https://jsonmock.hackerrank.com/api/movies';
 
 function App() {
+  const [apiQuery, setApiQuery] = useState('');
+
+  const onChange = useCallback((event) => {
+    const query = event.target.value;
+    console.log(query);
+    if (query.length > 0) {
+      setApiQuery(query);
+    }
+  }, []);
+
+  const getApiData = async () => {
+    await fetch(apiEndpoint, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(result => {
+        (async () => {
+          const stream = await result.body.getReader();
+
+          while (true) {
+            const { data, endOfStream } = await stream.read();
+
+            if (endOfStream) {
+              break;
+            }
+            console.log(data);
+
+          }
+        })();
+      })
+      .catch((error) => {
+        console.error(error.message)
+      });
+  }
+
+  useEffect(() => {
+    document.title = title;
+  });
+
+  useEffect(() => {
+    getApiData().then(result => {
+      console.log(result);
+    });
+
+
+  }, [apiQuery]);
+
   return (
     <div>
-      Welcome to Scout24 test
+      <input onChange={onChange}
+        type="number"
+        value={apiQuery} />
     </div>
   );
 }
